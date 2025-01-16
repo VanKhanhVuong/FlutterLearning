c4bef77b4d4452f1c17d97c5921cdbd2a5161a58
VanKhanhVuong <vankhanhvuong98@gmail.com>
2025-01-16 15:28:49 +0700

    commit adding a snackbar

--- flutter_project/flutter_rpg/lib/profile/profile.dart
+++ flutter_project/flutter_rpg/lib/profile/profile.dart
@@ -3,6 +3,7 @@ import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/profile/description_widget.dart';
 import 'package:flutter_rpg/profile/skill_list.dart';
 import 'package:flutter_rpg/profile/stats_table.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 
@@ -86,9 +87,21 @@ class Profile extends StatelessWidget {
               child: Column(
                 children: [StatsTable(character), SkillList(character)],
               ),
-            )
+            ),
 
             // Save Button
+            StyledButton(
+                onPressed: () {
+                  // show snackbar
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: const StyledHeading('Character saved.'),
+                    showCloseIcon: true,
+                    backgroundColor: AppColors.secondaryColor,
+                    duration: const Duration(seconds: 2),
+                  ));
+                },
+                child: const StyledHeading('save character')),
+            const SizedBox(height: 20),
           ],
         ),
       ),
