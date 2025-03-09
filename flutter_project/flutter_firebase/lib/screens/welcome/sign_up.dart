534fb879e387f1d913ce884311e6fee01e864ba3
VanKhanhVuong <vankhanhvuong98@gmail.com>
2025-03-09 20:22:12 +0700

    commit signing users up

--- flutter_project/flutter_firebase/lib/screens/welcome/sign_up.dart
+++ flutter_project/flutter_firebase/lib/screens/welcome/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth_tut/services/auth_service.dart';
 import 'package:flutter_auth_tut/shared/styled_button.dart';
 import 'package:flutter_auth_tut/shared/styled_text.dart';
 
@@ -70,7 +71,10 @@ class _SignUpFormState extends State<SignUpForm> {
                 if (_formKey.currentState!.validate()) {
                   final email = _emailController.text.trim();
                   final password = _passwordController.text.trim();
-                  print(email + password);
+
+                  final user = await AuthService.signUp(email, password);
+
+                  // Error Feedback
                 }
               },
               child: const StyledButtonText('Sign Up'),
